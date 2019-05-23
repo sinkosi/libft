a726ae6bf33c7fcb167f2ac80b2a2e696d29ac6c
Sibonelo NKOSI <sinkosi@c4r8s1.wethinkcode.co.za>
2019-05-23 14:39:23 +0200

    updated files for testing

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,11 +6,14 @@
 /*   By: sinkosi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/17 11:31:15 by sinkosi           #+#    #+#             */
-/*   Updated: 2019/05/21 14:22:27 by sinkosi          ###   ########.fr       */
+/*   Updated: 2019/05/23 14:23:37 by sinkosi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+int	ft_isupper(int c);
+int ft_islower(int c);
 
 int	ft_isalpha(int c)
 {
