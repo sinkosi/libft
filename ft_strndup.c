fc320afc78c9a2f6e72f62fd8bee8f6c970dfc73
Sibonelo NKOSI <sinkosi@c4r8s1.wethinkcode.co.za>
2019-05-30 14:58:24 +0200

    New files made 2019-05-30

---
+++ ft_strndup.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sinkosi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/30 11:03:38 by sinkosi           #+#    #+#             */
+/*   Updated: 2019/05/30 11:08:57 by sinkosi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strndup(const char *s1, size_t n)
+{
+	char	*dest;
+	if ((dest = (char *)malloc(ft_strlen(s1))))
+		return (ft_strncpy(dest, s1, n));
+	return (NULL);
+}
