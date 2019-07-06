931ba65619c790c0ab9b18fea311eaf5f487a199
sinkosi <sinkosi@student.wethinkcode.co.za>
2019-07-06 20:41:07 +0200

    These are files that need to be saved prior to a wildcard makefile being run

---
+++ l_ctype/ft_str_is_printable.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sinkosi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/24 07:43:22 by sinkosi           #+#    #+#             */
+/*   Updated: 2019/06/24 07:43:28 by sinkosi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "l_ctype.h"
+
+int	ft_str_is_printable(char *str)
+{
+	size_t	i;
+	int		control;
+
+	if (str == NULL)
+		return (1);
+	control = 1;
+	i = 0;
+	while (str[i] != '\0' && control == 1)
+	{
+		control = (ft_isprint(str[i]));
+		i++;
+	}
+	return (control);
+}
